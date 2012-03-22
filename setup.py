8571381c79f3f59c0a2c4c7d34380f88cb279322
Mikhail <glader.ru@gmail.com>
2012-03-23 00:00:25 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
     name='django_questions',
     description='Anonimous questions to site moderators. Not for wide use.',
+    version='0.0.1',
     packages=[
         'django_questions',
     ],
